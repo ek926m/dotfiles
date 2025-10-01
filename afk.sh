ddca36bc1682af6610b02bb855ad8c58276c166e
Eugen K. <ek926m@gmail.com>
2025-10-01 15:21:39 +0200

    Add files via upload

---
+++ afk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+caffeinate -d &
+while true;
+do
+   osascript -e 'tell application "Microsoft Teams" to activate'
+   osascript -e 'tell application "System Events" to keystroke "2" using {command down}'
+   #echo "refreshed"
+   sleep 290
+done
