033556050a3da51620aff60dfd7a25606bc96a1f
Eugen K. <ek926m@gmail.com>
2025-10-31 13:11:51 +0100

    Update afk.sh

--- afk.sh
+++ afk.sh
@@ -2,10 +2,21 @@
 caffeinate -d &
 while true;
 do
+   osascript -e 'tell application "System Events" to key code 103' 
+   sleep 1
+   osascript -e 'tell application "Terminal" to activate'
+   sleep 1
+   osascript -e 'tell application "DBeaver" to activate'
+   sleep 1
+   osascript -e 'tell application "Microsoft Outlook" to activate'
+   sleep 1
+   osascript -e 'tell application "Visual Studio Code" to activate'
+   sleep 1
+   osascript -e 'tell application "Google Chrome" to activate'
+   sleep 1
    osascript -e 'tell application "Microsoft Teams" to activate'
+   sleep 1
    osascript -e 'tell application "System Events" to keystroke "2" using {command down}'
-   #echo "refreshed"
-   sleep 290
-done
 
-# alias afk='/Users/ekai/afk.sh'
+   sleep 10
+done
