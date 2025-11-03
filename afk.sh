a9d8a89f3183022d14b1cc0ab8797fe27e7b9f8e
Eugen K. <ek926m@gmail.com>
2025-11-03 13:36:31 +0100

    Update afk.sh

--- afk.sh
+++ afk.sh
@@ -3,20 +3,23 @@ caffeinate -d &
 while true;
 do
    osascript -e 'tell application "System Events" to key code 103' 
-   sleep 1
+   sleep 0.25
    osascript -e 'tell application "Terminal" to activate'
-   sleep 1
+   sleep 0.25
    osascript -e 'tell application "DBeaver" to activate'
-   sleep 1
+   sleep 0.25
    osascript -e 'tell application "Microsoft Outlook" to activate'
-   sleep 1
+   sleep 0.25
    osascript -e 'tell application "Visual Studio Code" to activate'
-   sleep 1
+   sleep 0.25
    osascript -e 'tell application "Google Chrome" to activate'
-   sleep 1
+   sleep 0.25
    osascript -e 'tell application "Microsoft Teams" to activate'
-   sleep 1
+   sleep 0.25
    osascript -e 'tell application "System Events" to keystroke "2" using {command down}'
-
+   sleep 0.25
+   osascript -e 'tell application "System Events" to key code 103' 
+   sleep 0.25
+   osascript -e 'tell application "Terminal" to activate'
    sleep 10
 done
